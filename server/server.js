9392ecfcc18cd1c1d59ffde908b0f0ea9af82a7d
Rakesh <rakeshcse123@gmail.com>
2023-09-12 17:15:16 +0530

    MongoDB database connected succesfully

--- server/server.js
+++ server/server.js
@@ -2,13 +2,20 @@
 import  express  from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
 
 dotenv.config();
 
+// database config
+connectDB();
 
 // rest object
 const app = express();
 
+// middleware
+app.use(express.json());
+app.use(morgan("dev"));
 // rest api
 const PORT = process.env.PORT || 8080;
 
