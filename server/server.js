370435d877e7ec56c7a609486aca0f1ee3ab342b
Rakesh <rakeshcse123@gmail.com>
2023-09-12 17:50:48 +0530

    hashing password intigrate

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js';
 
 dotenv.config();
 
@@ -16,8 +17,12 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(morgan("dev"));
+
 // rest api
 const PORT = process.env.PORT || 8080;
+
+//routes
+app.use('/api/v1/auth',authRoutes);
 
 // Define a route
 app.get('/', (req, res) => {
